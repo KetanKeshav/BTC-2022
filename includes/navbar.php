75939555c957c720b6de7f10f2fcf154c6714cad
anzar00 <peerzada.anzar@gmail.com>
2022-09-06 00:09:36 +0530

    set-up-1

---
+++ includes/navbar.php
@@ -0,0 +1,42 @@
+    <!-- header-start -->
+    <header>
+        <div class="header-area ">
+            <div id="sticky-header" class="main-header-area ">
+                <div class="container-fluid p-0">
+                    <div class="row align-items-center justify-content-between no-gutters">
+                        <div class="col-xl-2 col-lg-2">
+                            <div class="logo-img">
+                                <a href="index.html">
+                                    <img src="img/navbar/logo.png" alt="">
+                                </a>
+                            </div>
+                        </div>
+                        <div class="col-xl-10 col-lg-10">
+                            <div class="main-menu  d-none d-lg-block">
+                                <nav>
+                                    <ul id="navigation">
+                                        <li><a class="active" href="index.html">Home</a></li>
+                                        <li><a href="Speakers.html">Speakers</a></li>
+                                        <li><a href="Schedule.html">About Us</a></li>
+                                        <li><a href="Schedule.html">Schedule</a></li>
+                                        <li><a href="Venue.html">Registration</a></li>
+                                        <li><a href="Venue.html">WIE Global Summit</a></li>
+                                        <li><a href="Venue.html">YP Global Summit</a></li>
+                                        <!-- <li><a href="#">pages <i class="ti-angle-down"></i></a>
+                                            <ul class="submenu">
+                                                <li><a href="elements.html">elements</a></li>
+                                            </ul>
+                                        </li> -->
+                                    </ul>
+                                </nav>
+                            </div>
+                        </div>
+                        <div class="col-12">
+                            <div class="mobile_menu d-block d-lg-none"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+    <!-- header-end -->
