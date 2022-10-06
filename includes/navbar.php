a52413fd2f3ef6356dd699db89ecdb6661032ec9
Ketan Keshav <ketan.keshav7@gmail.com>
2022-10-06 12:24:14 +0530

    Registration page updated

--- includes/navbar.php
+++ includes/navbar.php
@@ -21,7 +21,7 @@
                                         <li><a href="index.php#about">About Us</a></li>
                                         <li><a href="#">Schedule</a></li>
                                         <li><a <?php if($page=='venue') echo 'class="active"' ?> href="venue.php">Venue</a></li>
-                                        <li><a href="#">Registration</a></li>
+                                        <li><a <?php if($page=='registration') echo 'class="active"' ?> href="registration.php">Registration</a></li>
                                         <!-- <li><a href="Venue.html">WIE Global Summit</a></li>
                                         <li><a href="Venue.html">YP Global Summit</a></li> -->
                                         <!-- <li><a href="#">pages <i class="ti-angle-down"></i></a>
