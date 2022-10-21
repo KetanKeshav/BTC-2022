2b47cdfe156975b9a8a59052a2ad027a24b5c04c
anzar00 <peerzada.anzar@gmail.com>
2022-10-21 14:22:36 +0530

    speakers

--- includes/navbar.php
+++ includes/navbar.php
@@ -17,19 +17,19 @@
                                     <ul id="navigation">
                                         <li><a <?php if($page=='index') echo 'class="active"' ?> href="index.php">Home</a></li>
                                         <li><a <?php if($page=='oc') echo 'class="active"' ?> href="oc.php">Organizing Committee</a></li>
-                                        <!-- <li><a <?php if($page=='speakers') echo 'class="active"' ?> href="speakers.php">Speakers</a></li> -->
-                                        <li><a href="index.php#about">About Us</a></li>
-                                        <li><a <?php if($page=='schedule') echo 'class="active"' ?> href="schedule.php">Schedule</a></li>
+                                        <li><a <?php if($page=='speakers') echo 'class="active"' ?> href="speakers.php">Speakers</a></li>
                                         <li><a <?php if($page=='training') echo 'class="active"' ?> href="training.php">Metaverse Training</a></li>
+                                        <li><a <?php if($page=='schedule') echo 'class="active"' ?> href="#">About Us <i class="ti-angle-down"></i></a>
+                                            <ul class="submenu">
+                                                <li><a href="index.php#about">About Us</a></li>
+                                                <li><a <?php if($page=='schedule') echo 'class="active"' ?> href="schedule.php">Schedule</a></li>
+                                            </ul>
+                                        </li>
                                         <li><a <?php if($page=='venue') echo 'class="active"' ?> href="venue.php">Venue</a></li>
                                         <li><a <?php if($page=='registration') echo 'class="active"' ?> href="registration.php">Registration</a></li>
-                                        <!-- <li><a href="Venue.html">WIE Global Summit</a></li>
-                                        <li><a href="Venue.html">YP Global Summit</a></li> -->
-                                        <!-- <li><a href="#">pages <i class="ti-angle-down"></i></a>
-                                            <ul class="submenu">
-                                                <li><a href="elements.html">elements</a></li>
-                                            </ul>
-                                        </li> -->
+                                        <!-- <li><a href="Venue.html">WIE Global Summit</a></li> -->
+                                        <!-- <li><a href="Venue.html">YP Global Summit</a></li> -->
+                                        
                                     </ul>
                                 </nav>
                             </div>
