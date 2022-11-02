8a7a48bba183d0ba21d1a08c72f83a56ab055d0b
Ketan Keshav <ketan.keshav7@gmail.com>
2022-11-02 21:34:20 +0530

    Updated OC

--- oc.php
+++ oc.php
@@ -203,6 +203,42 @@
                     </div>
                 </div>
             </div>
+
+            <div class="row">
+                <div class="col-xl-3 col-md-6 single_speaker">
+                        <div class="speaker_thumb">
+                            <img src="img/oc/chengappa.png" alt="">
+                            <div class="hover_overlay">
+                                <div class="social_icon">
+                                    <a href="#"><i class="fa fa-facebook"></i></a>
+                                    <a href="#"><i class="fa fa-instagram"></i></a>
+                                    <a href="#"><i class="fa fa-twitter"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="speaker_name text-center">
+                            <h3>Chengappa M.R.</h3>
+                        </div>
+                </div>
+
+                <div class="col-xl-3 col-md-6 single_speaker">
+                    <div class="speaker_thumb">
+                        <img src="img/oc/abhishek.png" alt="">
+                        <div class="hover_overlay">
+                            <div class="social_icon">
+                                <a href="#"><i class="fa fa-facebook"></i></a>
+                                <a href="#"><i class="fa fa-instagram"></i></a>
+                                <a href="#"><i class="fa fa-twitter"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="speaker_name text-center">
+                        <h3>Abhishek Appaji</h3>
+                        <!-- <p>Designation</p> -->
+                    </div>
+                </div>
+
+            </div>
             <!-- end row -->
 
             <!-- row -->
